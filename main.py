dfc09c2b7f91241bcc7480734ac75765d04b97c1
opminsu <67817871+minsuinrealworld@users.noreply.github.com>
2022-01-31 09:06:29 +0000

    default setting

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def say_hello(a, b=0):
+import requests
+from bs4 import BeautifulSoup
 
-  print(a + b)
 
-say_hello(5, 2)
+indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85as_and=python&limit=50")
+
+indeed_soup = BeautifulSoup(indeed_result.text, 'lxml')
+
+
+
+#https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&radius=25&start=0
+# start 인자에 0 = 1gage, 50 = 2page
