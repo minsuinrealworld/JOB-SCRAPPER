28190dd07cfe1e9af72922643c83ff78d575c6c0
opminsu <67817871+minsuinrealworld@users.noreply.github.com>
2022-02-01 08:33:48 +0000

    vscode에서 작업하려고 푸시

--- indeed.py
+++ indeed.py
@@ -6,18 +6,42 @@ URL = f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}"
 last_page = 20
 
 
-def extract_indeed_pages():
-  result = req.get(URL)
-  soup = BeautifulSoup(URL, 'lxml')
+# def extract_indeed_pages():
+#   result = req.get(URL)
+#   soup = BeautifulSoup(URL, 'lxml')
   
-  return last_page
+#   return last_page
 
 
-#https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&radius=25&start=0
+#https://kr.indeed.com/jobs?q=python&limit=50&start=0
 # start 인자에 0 = 1gage, 50 = 2page
-def extract_indeed_jobs(last_page):
-  for page in range(last_page):
-
-    result = req.get(f"{URL}&start={page*LIMIT}")
-    print(result.status_code)
+def extract_indeed_jobs(last_page=last_page):
+  jobs = list()
+  # for page in range(last_page):
+  #   result = req.get(f"{URL}&start={page*LIMIT}")
+  #   soup = BeautifulSoup(result.text, "lxml")
+  #   results = soup("div", {"class": "mosaic-provider-jobcards"})
+  #   print(results)
+  #   return jobs
+  # Test code
+  result = req.get(f"{URL}&start={0*LIMIT}")
+  soup = BeautifulSoup(result.text, "lxml")
+  results = soup.find_all("a", {"class": "tapItem"})
   
+  for result in results:
+    temp = result.find('h2')
+    title = temp.find("span")['title']
+    print(title)
+    break
+    
+
+  
+  # for result in results:
+  #   #print(result.find("span", {"class": "title"}))
+  #   #print(result.find("span").find('title'))
+  #   #len(results) => 50
+  #   tab = result.find("h2")["jobTitle"]
+  #   print(tab)
+    
+  return jobs
+#div class: mosaic-provider-jobcards
